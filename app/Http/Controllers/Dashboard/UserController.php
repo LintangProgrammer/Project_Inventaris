11282b5e4f2eb791aba600610e4a9cdfc7d3d219
LintangProgrammer <lintangnurwijaya9@gmail.com>
2026-01-23 08:19:59 +0700

    Project

---
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $title = 'Delete User';
+        $text = 'Are you sure you want to delete?';
+        confirmDelete($title, $text);
+        $users = User::all();
+        return view('dashboard.users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('dashboard.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'required|string|in:admin,petugas',
+        ]);
+
+        $validated['password'] = bcrypt($validated['password']);
+        User::create($validated);
+
+        toast()->success('Success', 'User created successfully');
+        return redirect()->route('dashboard.users.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+
+    }
+
+
+    public function edit(string $id)
+    {
+        $user = User::findOrFail($id);
+        return view('dashboard.users.edit', compact('user'));
+    }
+
+   
+    public function update(Request $request, string $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
+            'password' => 'nullable|string|min:8|confirmed',
+            'role' => 'required|string|in:admin,petugas',
+        ]);
+
+       if ($request->filled('password')){
+        $request->validate([
+            'password' => 'required|string|min:8',
+        ]);
+        $validated['password'] = bcrypt($validated['password']);
+       }
+
+        $user->update($validated);
+        toast()->success('Success', 'User updated successfully');
+        return redirect()->route('dashboard.users.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        toast()->success('Success', 'User deleted successfully');
+        return redirect()->route('dashboard.users.index');
+    }
+}
